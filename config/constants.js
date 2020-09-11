7b628a5356013ddfc6ae54da6530b71cb1f651f0
Macbook <macbook@Macbooks-MacBook-Pro.local>
2020-09-11 22:50:01 +0500

    first commit

---
+++ config/constants.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI: "mongodb+srv://fayyaz:asdf@cluster0.iafx0.mongodb.net/MernTask?retryWrites=true&w=majority" ,
+    secretOrKey: "mysecret"
+  };
---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+const users = require("./routes/users");
+
+
+
+const app = express();
+
+//middlewares
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
+app.use(bodyParser.json());
+
+
+app.use(passport.initialize());
+
+require("./config/passport")(passport);
+
+// Routes
+app.use("/users", users);
+
+// DB Configuration
+const db = require("./config/constants").mongoURI;
+
+mongoose.connect(db,{ useNewUrlParser: true , useUnifiedTopology: true })
+  .then(() => console.log("MongoDB successfully connected"))
+  .catch(err => console.log(err));
+
+  //Error Middleware
+  app.use(function(req,res,next){
+    next(createError(404))
+  })
+
+  app.use(function(err,req,res,next){
+    res.locals.message = err.message
+    res.locals.error = req.app.get("env") === "development" ? err : {}
+    res.status(err.status || 500)
+    res.error(err)
+  })
+
+
+const port = process.env.PORT || 5000; 
+app.listen(port, () => console.log(`Server up and running on port ${port} !`));
